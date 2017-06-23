0eee4dee4da7d794864d9cf0d1907383076236b0
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2017-06-23 23:25:15 +0530

    Added a Repo on github.

--- main.py
+++ main.py
@@ -1,53 +1,105 @@
-import urllib, re
+import urllib
+import re
+import os
+import random
+import sys
 
-def is_full_link(link, return_selected_text=False):
-    """Returns if link is 'http://somewebsite.com/../someImage.png' style."""
-    matches = re.search('https?\:\/\/[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', link)
+
+# ONE LINERS
+# ====
+is_full         = lambda link: re.compile(r'.*?(https?\:\/\/[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)).*').findall(link)
+is_root_based   = lambda link: re.compile(r'.*?((?!\/\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)).*').findall(link)
+is_relative     = lambda link: re.compile(r'.*?((?!\/\/|\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)).*').findall(link)
+
+
+# FUNCTIONS
+# ====
+def random_name(length):
+    """Gets you a random filename."""
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_/"
+    name = ""
     
-    if matches != None && return_selected_text == False:
-        return True
-    elif matches != None && return_selected_text != False:
-        return matches.group(1)
-    else:
-        return False
+    for i in range(length):
+        name += chars[int(len(chars) * random.random())]
+    return name
+
+
+def get_extension(url):
+    """Gets the extension from URL."""
+    matches = re.match(r'.*?\.(png|jpeg|jpg|bmp|gif)', url)
+    return matches.group(1)
+    
+
+def download_image(url, image_name=None):
+    """Downloads an image."""
+    DIR_NAME = os.getcwd()+"/rj_images"
+    
+    if not os.path.exists(DIR_NAME):
+        os.makedirs(DIR_NAME)
         
-def is_root_relative_link(link, return_selected_text=False):
-    matches = re.search('(?!\/\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', link)
-    
-    if matches != None && return_selected_text == False:
-        return True
-    elif matches != None && return_selected_text != False:
-        return matches.group(1)
-    else:
-        return False
+    print "Downloading image from '%s'" % url,
 
-def is_normal_relative_link(link, return_selected_text=False):
-    matches = re.search('(?!\/\/|\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', link)
-    
-    if matches != None && return_selected_text == False:
-        return True
-    elif matches != None && return_selected_text != False:
-        return matches.group(1)
+    if image_name is None:
+        image_name = random_name(10)
     else:
-        return False
+        pass
+
+    try:        
+        image_save_location = DIR_NAME+"/"+image_name+"."+get_extension(url)
+        urllib.urlretrieve(url, image_save_location)
+        print " | Downloaded!"
+    except IOError as e:
+        print "| Error: " + e.strerror + " !!"
+        return None
+
+
+def append_links(links, array):
+    """List flattener"""
+    for link in links:
+        array.append(link)
+
+
+def get_all_links(line, BASE_URL):
+    """Gets all image links out of a line into an array."""
+    links = []    
+
+    # Matches http://blah.../some.png
+    if len(is_full(line)) > 0:
+        append_links(is_full(line), links)
+        
+    # Matches /blah.../some.png                
+    if len(is_root_based(line)) > 0:
+        temp_links = is_root_based(line)        
+        for i in range(len(temp_links)):
+            temp_links[i] = BASE_URL+temp_links[i]                
+        append_links(temp_links, links)
+        
+    # Matches blah.../some.png        
+    if len(is_relative(line)) > 0:
+        temp_links = is_relative(line)        
+        for i in range(len(temp_links)):
+            temp_links[i] = BASE_URL+"/"+temp_links[i]                
+        append_links(temp_links, links)        
+        
+    return links
     
 
-URL = "http://google.com"
+
+# MAIN
+# ===========
+
+try:
+    URL = sys.argv[1]
+except IndexError:
+    URL = raw_input("Give me a URL:")
+    #URL = "http://google.com" 
+
 my_page = urllib.urlopen(URL)
+all_the_links_of_the_world = []
 
 for line in my_page.readlines():
-    # Matches http://blah.../some.png
-    full_link_matches = re.search('https?\:\/\/[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', line)
-    # Matches /blah.../some.png
-    root_relative_link_matches = re.search('(?!\/\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', line)
-    # Matches blah.../some.png
-    relative_link_matches = re.search('(?!\/\/|\/)[a-zA-Z0-9_\-\/\.\?\=]*?\.(?:png|jpg|jpeg|gif|bmp)', line)
-    if full_link_matches != None:
-        print full_link_matches.group(0)
-    elif root_relative_link_matches != None:
-        print root_relative_link_matches.group(0)
-    elif relative_link_matches != None:
-        print relative_link_matches.group(0)
-            
+    links_for_this_line = get_all_links(line, URL)
+    append_links(links_for_this_line, all_the_links_of_the_world)
 
-
+for link in all_the_links_of_the_world:
+    download_image(link)
